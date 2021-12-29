e503f9840542c6b457b0c6a870f253a3e8156611
mp@mp <mp@mp.pl>
2021-12-29 14:26:44 +0100

    Service

---
+++ CKK.ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,17 @@
+﻿using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+
+namespace CKK.ViewModels
+{
+    public class MainWindowViewModel : ReactiveObject, IActivatableViewModel
+    {
+        [Reactive]
+        public string Greeting { get; set; } = "reactive model";
+        public MainWindowViewModel()
+        {
+
+        }
+
+        public ViewModelActivator Activator => new ViewModelActivator();
+    }
+}
