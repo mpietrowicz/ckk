4088fba5b052c1f2d75f1e710f94593267e6908f
Michal Pietrowicz <mp@mp.pl>
2024-10-03 20:17:15 +0200

    RM

--- CKK.WebService/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace CKK.WebService.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
